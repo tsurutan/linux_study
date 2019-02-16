cb2e1283abdc976f896b0f33e509238da4cb8b2c
tsurutan <ishaninaritaina623@gmail.com>
2019-02-16 13:12:36 +0900

    create fork process

--- fork-and-exec.c
+++ fork-and-exec.c
@@ -1,1 +1,31 @@
-#
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+static void child() {
+  char *args[] = { "/bin/echo", "hello", NULL };
+  printf("Im child! my pid is %d.\n", getpid());
+  fflush(stdout);
+  execve("/bin/echo", args, NULL);
+  err(EXIT_FAILURE, "exec() failed");
+}
+
+static void parent(pid_t pid_c) {
+  printf("Im parent! my pid is %d and the pid of my child is %d.\n",
+      getpid(), pid_c);
+  exit(EXIT_SUCCESS);
+}
+
+int main(void) {
+  pid_t ret;
+  ret = fork();
+  if (ret == -1)
+    err(EXIT_FAILURE, "fork() failed");
+  if (ret == 0) {
+    child();
+  } else {
+    parent(ret);
+  }
+  err(EXIT_FAILURE, "shouldnt reach here");
+}
